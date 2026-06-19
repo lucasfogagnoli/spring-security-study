7711b81ef296a1acc63a159ea9220cd04a7783ae
lucasfogagnoli <lucasfogagnoli@gmail.com>
2026-06-19 14:41:42 -0300

    Incluindo Spring Security, criando usuários de acesso, páginas de login e logout

---
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -0,0 +1,17 @@
+package med.voll.web_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String carregaPaginaLogin() {
+        return "autenticacao/login";
+    }
+
+    @GetMapping("/logout")
+    public String carregaPaginaLogout(){
+        return "autenticacao/logout";
+    }
+}
